16ef8fe34f180f9aeecf5c8c6ff14a244f20984f
Matías Méndez <70816257+mxtixsmendez@users.noreply.github.com>
2022-08-23 16:19:06 -0400

    Agrega metodos equals, hashcode y toString

--- Tarea 0/tarea0/CreativeWork.java
+++ Tarea 0/tarea0/CreativeWork.java
@@ -1,5 +1,7 @@
 package tarea0;
 
+import java.util.Objects;
+
 public class CreativeWork {
     private final String titulo;
     private final String copyright;
@@ -12,6 +14,27 @@ public class CreativeWork {
     public CreativeWork(String titulo){
         this.titulo = titulo;
         this.copyright = null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof CreativeWork)) return false;
+        CreativeWork that = (CreativeWork) o;
+        return titulo.equals(that.titulo) && Objects.equals(copyright, that.copyright);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(titulo, copyright);
+    }
+
+    @Override
+    public String toString() {
+        return "CreativeWork{" +
+                "titulo='" + titulo + '\'' +
+                ", copyright='" + copyright + '\'' +
+                '}';
     }
 
     public String getCopyright() {
